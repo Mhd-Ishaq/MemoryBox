7b1b79a8b353490cd9943a05e42b381cd1032aa0
Mhd-Ishaq <mdishaq3333@gmail.com>
2022-10-07 16:18:47 +0530

    make it better

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 const path = require("path");
 require('./db');
 const cors = require('cors');
