bc5586317bb07787d569354a67eb4b60187dbaa8
Mhd-Ishaq <mdishaq3333@gmail.com>
2023-02-16 16:45:13 +0530

    project completed

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -14,51 +14,10 @@ const Navbar = () => {
     navigate('/login');
   }
   return (
-    // <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-    //   <div className="container-fluid">
-    //     <Link className="navbar-brand" to="#">
-    //       iNoteBook
-    //     </Link>
-    //     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    //       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-    //         <li className="nav-item">
-    //           <NavLink
-    //             className="nav-link "
-    //             style={navListStyle}
-    //             aria-current="page"
-    //             to="/home"
-    //           >
-    //             Home
-    //           </NavLink>
-    //         </li>
-    //         {/* <li className="nav-item">
-    //           <NavLink className="nav-link" style={navListStyle} to="/about">
-    //             About
-    //           </NavLink>
-    //         </li> */}
-    //       </ul>
-    //       {
-    //         !localStorage.getItem('token') ?(
-          
-    //       <form className="d-flex">
-    //         <Link className="btn btn-primary mx-1" to="/login" role="button">
-    //           Login
-    //         </Link>
-    //         <Link className="btn btn-primary mx-1" to="/signup" role="button">
-    //           signup
-    //         </Link>
-    //       </form>
-    //         ):(
-    //           <div className="btn btn-danger " onClick={handleLogout}>Logout</div>
-    //         )
-    //       }
-    //     </div>
-    //   </div>
-    // </nav>
     <nav className="navbar">
       <div className="container-left">
         <Link  className="nav-link head button"  to="#">
-          iNoteBook
+        MemoryBox
         </Link>
             <NavLink
               className="nav-link button "
